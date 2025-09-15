0a9408dda232e53df73759ddcf5848deb3939f1d
kdevay <kat@wherewego.org>
2025-09-15 17:26:15 -0500

    chore: add proper key to mid-page nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -12,12 +12,12 @@ const NavBar: React.FC<NavBarProps> = ({ currentPage }) => {
   return (
     <nav className='border-y border-dotted md:flex hidden gap-2 items-center justify-center py-4 w-full'>
       {links.map((link, index) => (
-        <>
+        <div key={`${link.href}-mid-nav`}>
           <Link className='underline font-bold' href={link.href}>
             {link.label}
           </Link>
           {index < links.length - 1 && <p className='hidden md:block'>×</p>}
-        </>
+        </div>
       ))}
     </nav>
   );
