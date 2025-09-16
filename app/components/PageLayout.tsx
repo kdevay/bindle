cfafb110ccd8958b34a80737c2ead95ec527ade5
kdevay <kat@wherewego.org>
2025-09-16 13:38:31 -0500

    style: size-up main page heading

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -16,9 +16,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({
       <div className='font-sans flex flex-col min-h-screen min-w-screen p-4 md:p-8 pt-0 md:pt-0 gap-8 max-w-[1200px]'>
         <Header />
         <main className={`${contentStyles}`}>
-          {heading && (
-            <h1 className='text-2xl md:text-3xl font-bold'>{heading}</h1>
-          )}
+          {heading && <h1 className='text-3xl font-bold'>{heading}</h1>}
 
           {children}
         </main>
