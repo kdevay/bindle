bda809d640299895747d051c65ad2d3009c2cc70
kdevay <kat@wherewego.org>
2025-09-26 09:02:35 -0500

    style: fix max width styling

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -13,7 +13,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({
 }) => {
   return (
     <div className='flex justify-center items-center bg-natural'>
-      <div className='font-sans flex flex-col min-h-screen min-w-screen p-4 md:p-8 pt-0 md:pt-0 gap-8 max-w-[1200px]'>
+      <div className='font-sans flex flex-col min-h-screen w-full p-4 md:p-8 pt-0 md:pt-0 gap-8 lg:w-[1200px]'>
         <Header />
         <main className={`${contentStyles}`}>
           {heading && <h1 className='text-3xl font-bold'>{heading}</h1>}
